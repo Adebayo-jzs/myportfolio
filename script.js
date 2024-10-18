55cc8f2986045e16068c2d910feb8948ab5e8325
Adebayo-jzs <adedejiadebayo732@gmail.com>
2024-10-18 23:12:20 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    document.getElementById('offcanvas').classList.toggle('active');
+    document.getElementById('navLinks').classList.add('lin');
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('offcanvas').classList.remove('active');
+});
